174fa1b76fc19b5357eab0fc93b6f278bd9f444d
ddubuk228 <practice2280@gmail.com>
2026-01-19 22:46:08 +0900

    0011

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -1,13 +1,22 @@
 package com.back.domain.post.post.service;
 
+import com.back.domain.post.post.document.Post;
 import com.back.domain.post.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     public long count() {
         return postRepository.count();
+    }
+
+    public Post create(String title, String content, String author) {
+        Post post = new Post(title, content, author);
+        return postRepository.save(post);
+
     }
 }
